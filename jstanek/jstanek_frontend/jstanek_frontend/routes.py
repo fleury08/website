15d1af17ca2600cf4a528051699eb28abd1f3c6d
Jaroslav Staněk (Datalife) <j.stanek@datalife.cz>
2021-02-11 14:29:55 +0100

    aktualizace pyramid spoustece

--- jstanek/jstanek_frontend/jstanek_frontend/routes.py
+++ jstanek/jstanek_frontend/jstanek_frontend/routes.py
@@ -1,3 +1,7 @@
 def includeme(config):
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
+    config.add_route('password', '/tools/password')
+    config.add_route('base64', '/tools/base64')
+    config.add_route('hash', '/tools/hash')
+    config.add_route('mongo', '/tools/mongo')

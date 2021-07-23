208f81133409c464b0aa2a11cda83504a96070a5
Jaroslav Staněk (Datalife) <j.stanek@datalife.cz>
2021-07-23 13:46:18 +0200

    Projects removed

--- jstanek/jstanek_frontend/setup.py
+++ jstanek/jstanek_frontend/setup.py
@@ -24,7 +24,7 @@ tests_require = [
 
 setup(
     name='jstanek_frontend',
-    version='1.0.0',
+    version='1.0.1',
     description='jstanek frontend',
     long_description=README + '\n\n' + CHANGES,
     classifiers=[

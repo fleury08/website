82c86a8a37bdf5744a70f782971239416fbeb8dc
Jaroslav Staněk (Datalife) <j.stanek@datalife.cz>
2021-07-27 16:30:13 +0200

    WIP - parallax card

--- jstanek/jstanek_frontend/setup.py
+++ jstanek/jstanek_frontend/setup.py
@@ -24,7 +24,7 @@ tests_require = [
 
 setup(
     name='jstanek_frontend',
-    version='1.0.1',
+    version='1.0.2',
     description='jstanek frontend',
     long_description=README + '\n\n' + CHANGES,
     classifiers=[

87ddc44795df58650e658b8c94a1d0a2ecc29f14
Jaroslav Staněk <j.stanek@datalife.cz>
2024-03-14 16:15:37 +0100

    WIP - Running in Docker

--- jstanek/fe/vite.config.ts
+++ jstanek/fe/vite.config.ts
@@ -1,24 +1,30 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import Icons from 'unplugin-icons/vite';
 
-export default defineConfig({
-	plugins: [sveltekit(),
 
-    Icons({
-      compiler: 'svelte',
-    })],
-	server:{
-		proxy:{
-			"/api":
-				{
-					target: "http://localhost:8000/",
-					changeOrigin: true,
-					secure: false,
-					rewrite: (path) => path.replace(/^\/api/, '')
-				}
+export default defineConfig(({ mode }) => {
+	/* IMPORTANT: Load env variables from .env file
+	 * Plugins do not work without this, in this case Websocket plugin
+	 * won't load any svelte env variables from .env file
+	 */
+	const env = loadEnv(mode, process.cwd(), '');
+	return {
+		plugins: [sveltekit(),
+			Icons({
+				compiler: 'svelte'
+			})],
+		server: {
+			proxy: {
+				'/api':
+					{
+						target: env.ENV_BACKEND_URL ?? 'http://localhost:8000',
+						changeOrigin: true,
+						secure: false,
+						rewrite: (path) => path.replace(/^\/api/, '')
+					}
 
+			}
 		}
 	}
-
 });

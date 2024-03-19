635c562ecbf4ebf4326c195121518e0f57fc4f23
Jaroslav Staněk <jaroslav-stanek@pm.me>
2024-03-19 23:26:55 +0100

    responsivness fixes

--- jstanek/fe/src/lib/components/AboutMe.svelte
+++ jstanek/fe/src/lib/components/AboutMe.svelte
@@ -1,23 +1,29 @@
 <script>
-	import Contact from "./Contact.svelte";
-	import portrait from "$lib/assets/img/DSC_7787upBW_closeup.jpg";
-	import CoinParallax from "$lib/components/CoinParallax/CoinParallax.svelte";
+	import Contact from './Contact.svelte';
+	import portrait from '$lib/assets/img/DSC_7787upBW_closeup.jpg';
+	import CoinParallax from '$lib/components/CoinParallax/CoinParallax.svelte';
 </script>
+
 <div class="page">
-  <div class="container">
-    <div class="flex flex-col sm:flex-row gap-12 sm:gap-24 items-center">
-      <div class="flex flex-col gap-3 w-8/12 sm:w-4/12 ">
-          <CoinParallax image={portrait} borderless={false} />
-        <Contact />
-      </div>
-      <div class="align-self-center w-8/12">
-        <div class="p-3">
-          <div class="text-4xl md:text-8xl pb-6">Hello there!</div>
-          <div class="text-2xl md:text-3xl">It's me, <b>Jaroslav Staněk</b></div>
-          <p>Let me quickly introduce myself. I was born in 1989 and started exploring PCs at the age of 4. My first encounters were with the Commodore 64 and a 286 PC. After elementary school, I attended high school for four years and then university for three years, both specializing in networking. I am currently developing for a private company but am also working on my own projects.
-          </p>
-        </div>
-      </div>
-    </div>
-  </div>
+	<div class="container">
+		<div class="flex flex-col sm:flex-row gap-12 sm:gap-24 items-center">
+			<div class="flex flex-col gap-3 w-8/12 sm:w-4/12">
+				<CoinParallax image={portrait} borderless={false} />
+				<Contact />
+			</div>
+			<div class="align-self-center w-full sm:w-8/12">
+				<div class="p-1">
+					<div class="text-4xl md:text-8xl pb-6">Hello there!</div>
+					<div class="text-2xl md:text-3xl">It's me, <b>Jaroslav Staněk</b></div>
+					<p>
+						Let me quickly introduce myself. I was born in 1989 and started exploring PCs at the age
+						of 4. My first encounters were with the Commodore 64 and a 286 PC. After elementary
+						school, I attended high school for four years and then university for three years, both
+						specializing in networking. I am currently developing for a private company but am also
+						working on my own projects.
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
--- jstanek/fe/src/lib/components/Tools.svelte
+++ jstanek/fe/src/lib/components/Tools.svelte
@@ -17,26 +17,30 @@
 	import { Checkbox } from '$lib/components/ui/checkbox';
 	import { Textarea } from '$lib/components/ui/textarea';
 	import { Button } from '$lib/components/ui/button';
-	import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '$lib/components/ui/select';
+	import {
+		Select,
+		SelectContent,
+		SelectItem,
+		SelectTrigger,
+		SelectValue
+	} from '$lib/components/ui/select';
 
-	type SelectedHash = { name: string, value: string, default?: boolean }
+	type SelectedHash = { name: string; value: string; default?: boolean };
 	const hashes: SelectedHash[] = [
 		{ name: 'SHA-512', value: 'sha512', default: true },
 		{ name: 'SHA-256', value: 'sha256' },
-		{ name: 'SHA-1', value: 'sha1'},
-		{ name: 'MD5', value: 'md5'},
+		{ name: 'SHA-1', value: 'sha1' },
+		{ name: 'MD5', value: 'md5' }
 	];
-  const passSecurities = ['Basic', 'Medium', 'Strong', 'Secure']
-
-
+	const passSecurities = ['Basic', 'Medium', 'Strong', 'Secure'];
 
 	let passwordObject: PasswordOptions = defaultPasswordOptions();
 	let hashObject: HashObject = defaultHashObject();
 	let base64EncObject: Base64Object = defaultBase64Object();
 	let base64DecObject: Base64Object = defaultBase64Object();
 	let mongoDBObject: MongoDbObject = defaultMongoDbIdObject();
-	let selectedHash: SelectedHash = hashes.find(x => x.default) ?? hashes[0];
-	let selectedSecurity = "Strong";
+	let selectedHash: SelectedHash = hashes.find((x) => x.default) ?? hashes[0];
+	let selectedSecurity = 'Strong';
 	let generated = {
 		password: '',
 		hash: '',
@@ -44,7 +48,6 @@
 		base64encoded: '',
 		mongoTimestamp: ''
 	};
-
 
 	async function generatePasswordHandler() {
 		generated.password = await generatePassword(passwordObject);
@@ -54,11 +57,11 @@
 		generated.hash = await hashText(hashObject);
 	}
 
-	async function base64EncodeHandler(){
+	async function base64EncodeHandler() {
 		generated.base64encoded = await base64Encode(base64EncObject);
 	}
 
-	async function base64DecodeHandler(){
+	async function base64DecodeHandler() {
 		generated.base64decoded = await base64Decode(base64DecObject);
 	}
 
@@ -67,149 +70,155 @@
 	}
 
 	function passTemplateHandler(event: Event) {
-		selectedSecurity = (event.target as HTMLButtonElement).getAttribute("data-value") ?? 'Strong';
-		passwordObject = defaultPasswordOptions()
+		selectedSecurity = (event.target as HTMLButtonElement).getAttribute('data-value') ?? 'Strong';
+		passwordObject = defaultPasswordOptions();
 		switch (selectedSecurity) {
 			case 'Basic':
-				passwordObject.length = 8
-				passwordObject.special = false
-				passwordObject.numeric = false
+				passwordObject.length = 8;
+				passwordObject.special = false;
+				passwordObject.numeric = false;
 				break;
 			case 'Medium':
-				passwordObject.length = 12
-				passwordObject.special = false
+				passwordObject.length = 12;
+				passwordObject.special = false;
 				break;
 			case 'Strong':
 				break;
 			case 'Secure':
-				passwordObject.length = 24
+				passwordObject.length = 24;
 		}
 
-		return passwordObject
+		return passwordObject;
 	}
 
-	$:{
-		hashObject.alg = selectedHash.value
+	$: {
+		hashObject.alg = selectedHash.value;
 	}
 </script>
-
 
 <div class="page">
 	<div class="text-6xl text-center pb-10">Useful tools</div>
 	<div class="container">
-		<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-4">
+		<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
 			<Card>
 				<CardHeader>
 					<CardTitle>Password Generator</CardTitle>
 				</CardHeader>
 				<CardContent>
 					<div class="w-full mb-3 gap-3 flex-col flex">
-						<div class="flex  gap-1">
+						<div
+							class="flex max-sm:flex-col md:max-lg:flex-row lg:max-xl:flex-col xl:flex-row gap-1"
+						>
 							{#each passSecurities as passSecurity}
-								<Button class="w-full" variant="{selectedSecurity === passSecurity ? 'default' : 'outline'}" on:click={passTemplateHandler} data-value={passSecurity}>{passSecurity}</Button>
+								<Button
+									class="w-full"
+									variant={selectedSecurity === passSecurity ? 'default' : 'outline'}
+									on:click={passTemplateHandler}
+									data-value={passSecurity}>{passSecurity}</Button
+								>
 							{/each}
 						</div>
 						<div>
 							<Label for="passwordLength">Password length</Label>
-							<Input type="number"
-										 max="4096"
-										 id="passwordLength"
-										 bind:value={passwordObject.length} />
+							<Input
+								type="number"
+								max="4096"
+								id="passwordLength"
+								bind:value={passwordObject.length}
+							/>
 						</div>
 						<div>
-							<Checkbox bind:checked={passwordObject.uppercase}
-												id="passwordSubsetUppercase" />
+							<Checkbox bind:checked={passwordObject.uppercase} id="passwordSubsetUppercase" />
 							<Label for="passwordSubsetUppercase">Uppercase letters [A-Z]</Label>
 						</div>
 
 						<div>
-							<Checkbox bind:checked={passwordObject.lowercase}
-												id="passwordSubsetLowercase" />
+							<Checkbox bind:checked={passwordObject.lowercase} id="passwordSubsetLowercase" />
 							<Label for="passwordSubsetLowercase">Lowercase letters [a-z]</Label>
 						</div>
 						<div>
-							<Checkbox id="passwordSubsetNumbers"
-												bind:checked={passwordObject.numeric} />
+							<Checkbox id="passwordSubsetNumbers" bind:checked={passwordObject.numeric} />
 							<Label for="passwordSubsetNumbers">Numbers [0-9]</Label>
 						</div>
 						<div>
-							<Checkbox bind:checked={passwordObject.special}
-												id="passwordSubsetSymbols" />
+							<Checkbox bind:checked={passwordObject.special} id="passwordSubsetSymbols" />
 							<Label for="passwordSubsetSymbols">Special characters [$#/...]</Label>
 						</div>
-						<div><Label for="passwordOutput">Password</Label>
-							<Textarea rows={4}
-												id="passwordOutput" value={generated.password} /></div>
+						<div>
+							<Label for="passwordOutput">Password</Label>
+							<Textarea rows={4} id="passwordOutput" value={generated.password} />
+						</div>
 					</div>
 				</CardContent>
 				<CardFooter>
-					<Button class="w-full" id="passwordRegenerate"
-									on:click={generatePasswordHandler}
-					>Generate
+					<Button class="w-full" id="passwordRegenerate" on:click={generatePasswordHandler}
+						>Generate
 					</Button>
 				</CardFooter>
 			</Card>
 
-
 			<Card>
 				<CardHeader>
-					<CardTitle>
-						Hashing tool
-					</CardTitle>
+					<CardTitle>Hashing tool</CardTitle>
 				</CardHeader>
 				<CardContent>
 					<div class="w-full mb-3 gap-3 flex-col flex">
 						<div>
 							<Label for="hashInput">Input to hash</Label>
-							<Input
-								id="hashInput"
-								bind:value={hashObject.text} />
+							<Input id="hashInput" bind:value={hashObject.text} />
 						</div>
 						<div>
-							<Select  bind:selected={selectedHash} >
+							<Select bind:selected={selectedHash}>
 								<SelectTrigger>
-									<SelectValue placeholder="Select an algorithm, default is {hashes.find(x=>x.default)?.name}"/>
+									<SelectValue
+										placeholder="Select an algorithm, default is {hashes.find((x) => x.default)
+											?.name}"
+									/>
 								</SelectTrigger>
 								<SelectContent>
 									{#each hashes as hash}
-										<SelectItem value={hash.value} label={hash.name} >{hash.name}</SelectItem>
+										<SelectItem value={hash.value} label={hash.name}>{hash.name}</SelectItem>
 									{/each}
 								</SelectContent>
-
 							</Select>
 						</div>
 						<div>
-								<Textarea rows={5} id="hashOutput" value={generated.hash} />
+							<Textarea rows={5} id="hashOutput" value={generated.hash} />
 						</div>
 					</div>
 				</CardContent>
 				<CardFooter>
-					<Button class="w-full" on:click={hashTextHandler}
-					>Generate
-					</Button>
+					<Button class="w-full" on:click={hashTextHandler}>Generate</Button>
 				</CardFooter>
 			</Card>
 
 			<Card>
 				<CardHeader>
-					<CardTitle>
-						Base64
-					</CardTitle>
+					<CardTitle>Base64</CardTitle>
 				</CardHeader>
 				<CardContent>
 					<div class="w-full mb-3 gap-3 flex-col flex">
 						<div>
 							<Label for="base64EncodeInput">Encode</Label>
-							<Input id="base64EncodeInput" type="text" on:keyup={base64EncodeHandler} bind:value={base64EncObject.text}/>
+							<Input
+								id="base64EncodeInput"
+								type="text"
+								on:keyup={base64EncodeHandler}
+								bind:value={base64EncObject.text}
+							/>
 						</div>
 						<div>
 							<Textarea id="base64EncodeOutput" value={generated.base64encoded} />
 						</div>
 
 						<div>
-							<Label for="base64DecodeInput"
-										 class="form-label">Decode</Label>
-							<Input id="base64DecodeInput" type="text" on:keyup={base64DecodeHandler} bind:value={base64DecObject.text} />
+							<Label for="base64DecodeInput" class="form-label">Decode</Label>
+							<Input
+								id="base64DecodeInput"
+								type="text"
+								on:keyup={base64DecodeHandler}
+								bind:value={base64DecObject.text}
+							/>
 						</div>
 						<div>
 							<Textarea id="base64DecodeOutput" value={generated.base64decoded} />
@@ -220,35 +229,32 @@
 
 			<Card>
 				<CardHeader>
-					<CardTitle>
-						MongoDB _id tool
-					</CardTitle>
+					<CardTitle>MongoDB _id tool</CardTitle>
 				</CardHeader>
 				<CardContent>
 					<div class="w-full mb-3 gap-3 flex-col flex">
 						<div>
 							<Label for="mongoDbIdInput">_id</Label>
-							<Input id="mongoDbIdInput" type="text" on:keyup={mongoIdConvertHandler} bind:value={mongoDBObject.object_id}/>
+							<Input
+								id="mongoDbIdInput"
+								type="text"
+								on:keyup={mongoIdConvertHandler}
+								bind:value={mongoDBObject.object_id}
+							/>
 						</div>
 						<div>
 							<Label for="mongoDbIdOutput">Timestamp</Label>
-							<Input type="text" id="mongoDbIdOutput" value={generated.mongoTimestamp}/>
+							<Input type="text" id="mongoDbIdOutput" value={generated.mongoTimestamp} />
 						</div>
 					</div>
 				</CardContent>
-				<CardFooter>
-				</CardFooter>
+				<CardFooter />
 			</Card>
 
 			<Card>
 				<CardHeader />
-				<CardContent>
-					...more to come.
-				</CardContent>
+				<CardContent>...more to come.</CardContent>
 			</Card>
 		</div>
 	</div>
 </div>
-
-
-

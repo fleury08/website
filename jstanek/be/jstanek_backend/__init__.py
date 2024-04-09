222c3f534b66e1963575f4a4718292a210a5bc00
Jaroslav Staněk <jaroslav-stanek@pm.me>
2024-04-09 13:14:05 +0200

    WIP - tests

--- jstanek/be/jstanek_backend/__init__.py
+++ jstanek/be/jstanek_backend/__init__.py
@@ -4,7 +4,16 @@ from fastapi import FastAPI
 from .root import router_api
 from .websockets import router_ws
 
-logging.basicConfig(level=logging.DEBUG)
-app: FastAPI = FastAPI()
-app.include_router(router_ws)
-app.include_router(router_api)
+app = None
+
+
+def construct_app():
+    logging.basicConfig(level=logging.DEBUG)
+    _app: FastAPI = FastAPI()
+    _app.include_router(router_ws)
+    _app.include_router(router_api)
+    return _app
+
+
+if __name__ == "__main__":
+    app = construct_app()
